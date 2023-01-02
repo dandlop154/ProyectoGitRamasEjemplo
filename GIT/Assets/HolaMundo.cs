44f3f751d0fb6861d3ae2c4f329f90bd069a33cc
dandlop154 <dandlop154@users.noreply.github.com>
2023-01-02 19:13:20 +0100

    Update HolaMundo.cs

--- GIT/Assets/HolaMundo.cs
+++ GIT/Assets/HolaMundo.cs
@@ -7,7 +7,7 @@ public class HolaMundo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello Mundo");
+        Debug.Log("Hola Mundo");
     }
 
     // Update is called once per frame
